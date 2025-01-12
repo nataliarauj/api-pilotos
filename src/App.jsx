d1e3231f9543c8924b0f41e6653da60fdb5f0ef8
Cláudio Crispim Campos Júnior <105263167+claudi0jr@users.noreply.github.com>
2025-01-12 19:59:22 -0300

    Perf: troca da API-Key

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     try {
       const response = await axios.get("https://v1.formula-1.api-sports.io/drivers", {
         headers: {
-          "x-rapidapi-key": "913b3caecdbfca7b9472150855f3f9ee", 
+          "x-rapidapi-key": "70d6ef901f84a9596c55ba95b131f50a", 
           "x-rapidapi-host": "v1.formula-1.api-sports.io",
         },
         params: {
