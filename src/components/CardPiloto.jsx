e91a7fbbeef447661c616a72eb37e4d764c44759
Cláudio Crispim Campos Júnior <105263167+claudi0jr@users.noreply.github.com>
2025-01-13 20:37:39 -0300

    Refactor: alterado botao de Adicionar favoritos, para que fique em Remover Favoritos apos ser utilizado.

--- src/components/CardPiloto.jsx
+++ src/components/CardPiloto.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 
 const CardPiloto = ({ piloto, adicionarAosFavoritos, favoritos, removerFavorito }) => {
-
   const isFavorito = favoritos.some((fav) => fav.driverId === piloto.driverId);
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
-  <img 
-    src={piloto.image} 
-    alt={`Foto de ${piloto.name}`} 
-    className="w-24 h-24 rounded-full mb-4"
-  />
-  <h3 className="text-lg font-bold">{piloto.name}</h3>
-  <p className="text-gray-600">Nacionalidade: {piloto.nationality}</p>
-  <p className="text-gray-600">Pódios: {piloto.podiums}</p>
-  <button 
-    className="bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-600"
-    onClick={() => adicionarAosFavoritos(piloto)}
-  >
-    Adicionar aos Favoritos
-  </button>
-</div>
-
+      <img 
+        src={piloto.image} 
+        alt={`Foto de ${piloto.name}`} 
+        className="w-24 h-24 rounded-full mb-4"
+      />
+      <h3 className="text-lg font-bold">{piloto.name}</h3>
+      <p className="text-gray-600">Nacionalidade: {piloto.nationality}</p>
+      <p className="text-gray-600">Pódios: {piloto.podiums}</p>
+      
+      {isFavorito ? (
+        <button 
+          className="bg-red-500 text-white px-4 py-2 rounded mt-4 hover:bg-red-600"
+          onClick={() => removerFavorito(piloto)}
+        >
+          Remover dos Favoritos
+        </button>
+      ) : (
+        <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-600"
+          onClick={() => adicionarAosFavoritos(piloto)}
+        >
+          Adicionar aos Favoritos
+        </button>
+      )}
+    </div>
   );
 };
 
