3b0f4e1362f8f53b80fcf1330dc01998f8defd09
Cláudio Crispim Campos Júnior <105263167+claudi0jr@users.noreply.github.com>
2025-01-15 13:56:08 -0300

    refactor: removido linha da idade dos pilotos & equipe

--- src/components/CardPiloto.jsx
+++ src/components/CardPiloto.jsx
@@ -1,34 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./CardPiloto.css";
 
-const CardPiloto = ({ piloto, adicionarAosFavoritos, favoritos, removerFavorito }) => {
-  const [isFavorito, setIsFavorito] = useState(false); // Estado local para verificar se é favorito
+const CardPiloto = ({ piloto, adicionarAosFavoritos, favoritos }) => {
+  const [isFavorito, setIsFavorito] = useState(false);
 
-  // Verifica se o piloto é favorito sempre que a lista de favoritos for atualizada
   useEffect(() => {
     setIsFavorito(favoritos.some((fav) => fav.driverId === piloto.driverId));
-  }, [favoritos, piloto.driverId]);  // Atualiza se a lista de favoritos mudar
+  }, [favoritos, piloto.driverId]);
 
   return (
     <div className="card-container">
       <div className="card">
-      <img
-        src={piloto.image}
-        alt={`Foto de ${piloto.name}`}
-        className="w-24 h-24 rounded-full mb-4"
-      />
-    
-      <h3 className="text-lg font-bold">{piloto.name}</h3>
-      <p className="text-gray-600">Nacionalidade: {piloto.nationality}</p>
-      <p className="text-gray-600">Pódios: {piloto.podiums}</p>
-      <p className="text-gray-600">Equipe Atual: {piloto.currentTeam}</p>
-      <p className="text-gray-600">Idade: {piloto.age} anos</p>
+        <img
+          src={piloto.image}
+          alt={`Foto de ${piloto.name}`}
+          className="w-24 h-24 rounded-full mb-4"
+        />
 
-      <button onClick={() => adicionarAosFavoritos(piloto)}>
+        <h3 className="text-lg font-bold">{piloto.name}</h3>
+        <p className="text-gray-600">Nacionalidade: {piloto.nationality}</p>
+        <p className="text-gray-600">Pódios: {piloto.podiums}</p>
+
+        <button onClick={() => adicionarAosFavoritos(piloto)}>
           Adicionar aos Favoritos
-      </button>
+        </button>
+      </div>
     </div>
-  </div>
   );
 };
 
